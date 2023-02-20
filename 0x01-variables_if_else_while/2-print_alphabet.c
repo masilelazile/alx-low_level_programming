4bf5f9ba5e4a3277c9cebf0e8e575ad91972d3da
masilelazile <masilelazile@gmail.com>
2023-02-20 14:50:42 -0800

    3-print_alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 /**
  * main - Entry point
- *
+ * Description: 'Print alphabets lowercase'
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	char c;
-	c = 'a';
-	while
-
-		(c <= 'z'){
-			putchar(c);
-			c++;
-		}
 	putchar('\n');
 	return (0);
 }
