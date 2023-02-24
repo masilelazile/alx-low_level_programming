76ba4cd261d70343a73aaae08345a7b24ed17252
masilelazile <masilelazile@gmail.com>
2023-02-24 13:28:41 -0800

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,18 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * main - Prints a Fizz Buzz program
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int x;
 
 	for (x = 1; x <= 100; x++)
 	{
-	if ((x % 3 == 0) && (x % 5 == ))
+	if ((x % 3 == 0) && (x % 5 == 0))
 	{
 	printf("FizzBuzz");
 	}
